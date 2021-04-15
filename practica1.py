b0049c78e53f418471ee39fa7a1a2e6339cd23f3
juanM20 <jmtp12poke@gmail.com>
2021-04-14 23:10:31 -0500

    Pr

--- practica1.py
+++ practica1.py
@@ -5,6 +5,7 @@ from PIL import Image
 import cv2
 import numpy as np 
 from matplotlib import pyplot as plt
+import pandas as pd
 
 
 st.title("Abre una imagen :3")
@@ -14,9 +15,19 @@ original = Image.open('Imagen3.jpg')
 st.image(original, use_column_width=True)
 
 image = cv2.imread('Imagen3.jpg',0)
-st.map(plt.hist(image.ravel(), 256,[0,256]))
+histograma =  plt.hist(image.ravel(), 256,[0,256])
+plt.show()
 
-print("Huevos Pepitas y cacahuates")
+
+segunda = cv2.equalizeHist(original)
+st.image(segunda, use_column_width=True)
+
+histograma2 =  plt.hist(segunda.ravel(), 256,[0,256])
+plt.show()
+
+
+
+
 
 
 
