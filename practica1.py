bf3477489717736060cf14d5dcb51372ad671d1d
juanM20 <jmtp12poke@gmail.com>
2021-04-14 22:49:40 -0500

    Imagen

--- practica1.py
+++ practica1.py
@@ -1,2 +1,31 @@
-print('Hola Jorge así se usa VsCode :3')
+
+
+import streamlit as st
+from PIL import Image
+import cv2
+import numpy as np 
+from matplotlib import pyplot as plt
+
+
+st.title("Abre una imagen :3")
+
+original = Image.open('Imagen3.jpg')
+
+st.image(original, use_column_width=True)
+
+image = cv2.imread('Imagen3.jpg',0)
+st.map(plt.hist(image.ravel(), 256,[0,256]))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
