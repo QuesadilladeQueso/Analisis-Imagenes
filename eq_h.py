01ab5304c3213ce1f563bab7edab55dbb3c70645
Quesa <johndoe@example.com>
2021-04-17 14:19:02 -0500

    Lo que hizo jorge, pero en 3 lineas

--- eq_h.py
+++ eq_h.py
@@ -1,6 +1,5 @@
 
 
-import streamlit as st
 from PIL import Image
 import cv2
 import numpy as np 
@@ -20,6 +19,25 @@ cv2.imshow('Imagen 3', image)
 hist = cv2.calcHist([image], [0], None, [256], [0,256])
 
 print(hist)
+plt.plot(hist)
+
+largo, alto = image.shape
+ 
+for l in range(largo):
+    for a in range(alto):
+            #print("antes del cambio", image[l,a])
+            image[l,a] = image[l,a] + 100
+            #print("despues del cambio", image[l,a])
+print('-----------------')
+
+hist2 = cv2.calcHist([image], [0], None, [256], [0,256])
+
+print(hist2)
+plt.plot(hist2)
+
+cv2.imshow("window",image)
+cv2.waitKey(0)
+
 # plt.plot(hist, color='gray')
 
 # plt.xlabel('intensidad de iluminacion')
