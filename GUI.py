83521139b245a6e984b30cc4b9a6b9feef64eafe
juanM20 <jmtp12poke@gmail.com>
2021-04-18 10:40:06 -0500

    GUI

--- GUI.py
+++ GUI.py
@@ -1,18 +1,47 @@
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+from tkinter import filedialog
+import cv2
 
-import tkinter
+def Seleccion_Imagen(panel):
+    
+    ruta = filedialog.askopenfilename(filetypes= {
+        ("image", ".jpeg"),
+        ("image", ".png"),
+        ("image", ".jpg")
+    })
+    
+    if len(ruta) > 0:
+        
+        image = cv2.imread(ruta)
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        image = Image.fromarray(image)
+        
+        image = ImageTk.PhotoImage(image)
 
-ventana = tkinter.Tk()
-ventana.geometry("600x600")
-
-etiqueta = tkinter.Label(ventana, text="Practica 1")
-etiqueta.pack()
-
-def saludo():
-    print("Hola "+nombre)
-
-boton1 = tkinter.Button(ventana, text="Aceptar", padx = 20, pady = 15, command = lambda: saludo)
-boton1.pack()
-
-ventana.mainloop()
+        if panel is None:
+            
+            panel = Label(image=image)
+            panel.image = image
+            panel.pack(side = "top",padx=10, pady=10)
+            
+        else:
+            
+            panel.configure(image=image)
+            panel.image = image
 
 
+if __name__ == '__main__':
+    
+    raiz = Tk()
+    panel = None
+    
+    
+    btn = Button(raiz, text="Selecciona una imágen", command=Seleccion_Imagen(panel) )
+    btn.pack(fill="both", expand="yes", padx="10", pady="10")
+    
+    raiz.mainloop()
