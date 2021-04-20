007fbc3ca3c2e1fd96291d0356ad24b81c05c9fd
Quesa <johndoe@example.com>
2021-04-19 21:19:06 -0500

    Cmabios tontos

--- eq_h.py
+++ eq_h.py
@@ -1,6 +1,7 @@
 
 import cv2
 import numpy as np 
+import matplotlib.cm as cm
 from matplotlib import pyplot as plt
 
 
@@ -13,18 +14,32 @@ from matplotlib import pyplot as plt
 image = cv2.imread('Imagen3.jpg',0)
 # cv2.imshow('Imagen 3', image)
 
-
 hist = cv2.calcHist([image], [0], None, [256], [0,256])
 
+def Proba(pixel, hist):
+    return hist[i]/31376
 # print(hist)
+matriz = np.array(image)
+M_matriz = []
+# M_matriz = matriz.reshape(1,31376)
+c = 0 
+d = 0
 
-largo, alto = image.shape
- 
-# for l in range(largo):
-#     for a in range(alto):
-#             #print("antes del cambio", image[l,a])
-#             image[l,a] = image[l,a] + 100
-#             #print("despues del cambio", image[l,a])
+
+for i in matriz:
+    d = Proba(i, hist)
+    c = c + d
+    a = i - c
+    M_matriz.append(a)
+    
+
+           
+
+             
+
+plt.imsave('filename.png', np.array(M_matriz).reshape(212,148), cmap=cm.gray)
+print("despues del cambio")
+Image. open('filename.png')
 # # print('-----------------')
 
 # hist2 = cv2.calcHist([image], [0], None, [256], [0,256])
@@ -40,17 +55,16 @@ largo, alto = image.shape
 # plt.xlabel('intensidad de iluminacion')
 # plt.ylabel('cantidad de pixeles')
 # plt.show()
-def Proba(pixel, hist):
-    return hist[pixel]/31376
+
 
 
 matriz = np.array(image)
 n_matriz = matriz.reshape(1,31376)
 
 
-for pixel in n_matriz:
-    print("pixel: ",pixel)
-    print("probabilidad: ", Proba(pixel, hist))
+#for pixel in n_matriz:
+ #   print("pixel: ",pixel)
+  #  print("probabilidad: ", Proba(pixel, hist))
 
 # for x in n_matriz:
 #     print(x
